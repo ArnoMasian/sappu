13c08e67b637e17bb8df2d1d68704c78b6d33f2e
ArnoMasian <hnaisam@gmail.com>
2023-08-09 23:11:37 +0500

    Added random rolls to right sidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -5,7 +5,15 @@ import UserCard from "../cards/UserCard";
 import { fetchCommunities } from "@/lib/actions/community.actions";
 import { fetchUsers } from "@/lib/actions/user.actions";
 
-async function RightSidebar() {
+function shuffleArray<T>(array: T[]): T[] {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+async function RightSidebar(): Promise<JSX.Element | null> {
   const user = await currentUser();
   if (!user) return null;
 
@@ -14,7 +22,20 @@ async function RightSidebar() {
     pageSize: 4,
   });
 
-  const suggestedCOmmunities = await fetchCommunities({ pageSize: 4 });
+  const randomSimilarMinds = shuffleArray(similarMinds.users);
+
+  const limitedRandomSimilarMinds = randomSimilarMinds.slice(0, 10);
+
+  const suggestedCommunities = await fetchCommunities({ pageSize: 4 });
+
+  const randomSuggestedCommunities = shuffleArray(
+    suggestedCommunities.communities
+  );
+
+  const limitedRandomSuggestedCommunities = randomSuggestedCommunities.slice(
+    0,
+    10
+  );
 
   return (
     <section className="custom-scrollbar rightsidebar">
@@ -24,9 +45,9 @@ async function RightSidebar() {
         </h3>
 
         <div className="mt-7 flex w-[350px] flex-col gap-9">
-          {suggestedCOmmunities.communities.length > 0 ? (
+          {limitedRandomSuggestedCommunities.length > 0 ? (
             <>
-              {suggestedCOmmunities.communities.map((community) => (
+              {limitedRandomSuggestedCommunities.map((community) => (
                 <UserCard
                   key={community.id}
                   id={community.id}
@@ -48,9 +69,9 @@ async function RightSidebar() {
       <div className="flex flex-1 flex-col justify-start">
         <h3 className="text-heading4-medium text-light-1">Similar Minds</h3>
         <div className="mt-7 flex w-[350px] flex-col gap-10">
-          {similarMinds.users.length > 0 ? (
+          {limitedRandomSimilarMinds.length > 0 ? (
             <>
-              {similarMinds.users.map((person) => (
+              {limitedRandomSimilarMinds.map((person) => (
                 <UserCard
                   key={person.id}
                   id={person.id}
